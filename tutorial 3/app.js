e4627bfa3ef0e91e8c819d7ce64bfc77d1b79c65
Abror <135578437+eagleabror@users.noreply.github.com>
2023-10-27 20:33:47 +0500

    tutorial 3 varibles

---
+++ tutorial 3/app.js
@@ -0,0 +1,17 @@
+let name = 'Abror';
+let age = 24
+console.log(name, age);
+
+yosh = 26
+
+console.log(yosh)
+
+const surname = 'Abdukayumov'
+
+console.log(surname)
+
+var loc = 'Tashkent'
+
+console.log(loc)
+
+console.log('Abror')
