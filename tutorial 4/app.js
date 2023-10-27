265a0d2199637448b4111448a22b382aee07f39b
Abror <135578437+eagleabror@users.noreply.github.com>
2023-10-27 23:59:23 +0500

    string method

---
+++ tutorial 4/app.js
@@ -0,0 +1,68 @@
+// string 
+console.log('Hello everyone!');
+
+let email = 'abrorkhandeveloper@gmail.com';
+
+console.log(email);
+
+// string concatenation
+
+let fristName = 'Abror';
+
+let lastName = 'Abdukayumov'
+
+let fullName = fristName + ' ' + lastName
+
+console.log(fullName);
+
+// getting characters
+
+console.log(fullName[0]);
+
+// string length => uzunlik
+
+console.log(fullName.length)
+
+// string methods => funktsiyaga o'xshash ish bajaradi  
+
+console.log(fullName.toUpperCase())
+
+// let result = fullName.toLowerCase()
+
+// console.log(result)
+
+let index = email.indexOf('@')
+
+console.log(index)
+
+/* ko'p ishlatilinadigan string metodlar */
+
+let mail = "abrorkhandeveloper@gmail.com"
+
+// lastIndexOf()
+
+/* let result = mail.lastIndexOf('a')
+
+console.log(result) */
+
+// slice()
+
+/* let result = mail.slice(9, 10)
+
+console.log(result) */
+
+// substr()
+
+/* let result = mail.substr(4, 10)
+
+console.log(result) */
+
+// replace()
+
+// charAt()
+
+// toUpperCase() - tolowerCase()
+
+// trim()
+
+// splice()
